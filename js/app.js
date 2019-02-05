ca3420995a69d0dface7a8dc3f2dc186e66ff809
Sourav Dey <souravtukai21@gmail.com>
2019-02-06 00:50:53 +0530

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-var app = angular.module("myShoppingList", []); 
-app.controller("myCtrl", function($scope) {
-    $scope.products = ["Milk", "Bread", "Cheese"];
+var app = angular.module("cvapp", []); 
+app.controller("genericController", function($scope,$http) {
+    $scope.products = [];
+    $http.get('https://yedvaruos.github.io/model/details.json').then(function(response) {
+        $scope.products = response.data;
+     });
+    
     $scope.addItem = function () {
         $scope.products.push($scope.addMe);
     }    
